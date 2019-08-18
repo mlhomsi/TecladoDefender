a9267fc057d048c7ef6b7075b4b6b1fb9d2c7274
mlhomsi <mlhomsi@gmail.com>
2019-08-18 00:18:18 -0300

    Alfa Navegabilidade

---
+++ TecladoDefender/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Quit();
+    }
+}
